a2539ca3f7a990b086eadff13212c0ab59fe3e4c
Marko Vejnovic <marko@plocca.com>
2026-02-05 18:24:33 -0800

    style

--- Formula/git-fs.rb
+++ Formula/git-fs.rb
@@ -1,8 +1,8 @@
 class GitFs < Formula
   desc "Mount Mesa, GitHub and GitLab repositories as local filesystems via FUSE"
   homepage "https://github.com/mesa-dot-dev/git-fs"
-  version "0.1.2-alpha.1"
   url "https://github.com/mesa-dot-dev/git-fs/releases/download/v0.1.2-alpha.1/git-fs-macos-universal.tar.gz"
+  version "0.1.2-alpha.1"
   sha256 "1893ee1f78f0e087ff1e0952d1395a3c077667d13677d4b78588050cf3e3cfd8"
   license "MIT"
 
