c2f18018f8b1085755fdafaee6c22010ad932bbc
Peter Rallojay <Peterrallojay@users.noreply.github.com>
2026-02-10 14:30:43 -0800

    code-review 0.0.4

--- Formula/code-review.rb
+++ Formula/code-review.rb
@@ -1,31 +1,15 @@
 class CodeReview < Formula
   desc "Mesa code review CLI"
   homepage "https://mesa.dev"
-  version "0.0.3"
+  url "https://github.com/mesa-dot-dev/homebrew-tap/releases/download/mesa-code-review-v0.0.4/mesa-code-review-0.0.4.tgz"
+  sha256 "a4c9a877778ce99a23de11e093faf99bf948f8b6a2fa74f673f8fd82f68e56a2"
   license "Apache-2.0"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/mesa-dot-dev/homebrew-tap/releases/download/mesa-code-review-v0.0.3/mesa_0.0.3_darwin_arm64.tar.gz"
-      sha256 "6f850e1d17067bfdff793e673eadb691d8bf5bfec1dc5e026d37032374858537"
-    else
-      url "https://github.com/mesa-dot-dev/homebrew-tap/releases/download/mesa-code-review-v0.0.3/mesa_0.0.3_darwin_x64.tar.gz"
-      sha256 "6f5c23cdd5c3475adf7902bac655207679316ca73f47ca245ec5538f6376ba01"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/mesa-dot-dev/homebrew-tap/releases/download/mesa-code-review-v0.0.3/mesa_0.0.3_linux_arm64.tar.gz"
-      sha256 "3477c4ee1f1ca271595f5740a63c8749a80f14928886532e15fe61b89246528d"
-    else
-      url "https://github.com/mesa-dot-dev/homebrew-tap/releases/download/mesa-code-review-v0.0.3/mesa_0.0.3_linux_x64.tar.gz"
-      sha256 "d2ea054eff1bfea0cc9a76463fb4139d4ad3249d60b66518552d761d951b11a9"
-    end
-  end
+  depends_on "node"
 
   def install
-    bin.install "mesa"
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
   end
 
   test do
