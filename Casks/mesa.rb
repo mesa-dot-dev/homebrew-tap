d8bb208edb1703568c46774c200d584b911948fc
Ben Warren <warrenbhw@gmail.com>
2026-07-12 18:42:13 -0700

    Fix Mesa cask homepage style

--- Casks/mesa.rb
+++ Casks/mesa.rb
@@ -5,7 +5,7 @@ cask "mesa" do
   url "https://packages.buildkite.com/mesa-dot-dev/files-public/files/mesa-macos-arm64-0.40.0.zip"
   name "Mesa"
   desc "Mount Mesa repositories as local filesystems via FUSE"
-  homepage "https://mesa.dev"
+  homepage "https://mesa.dev/"
 
   depends_on arch: :arm64
   depends_on cask: "mesa-dot-dev/tap/fuse-t"
